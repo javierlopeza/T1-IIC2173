bcb4e29dc295891cb9cb6633c821623eb5999f6f
Javier López Achondo <jilopez8@uc.cl>
2017-08-30 15:08:23 -0300

    List requests

---
+++ app/views/registers/_register.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! register, :id, :date, :client_ip, :created_at, :updated_at
+json.url register_url(register, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :registers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "registers#index"
 end
